6980c48db4d23d296fb24f09106a7bd6ae4da737
Keshav Gupta <keshav1032@gmail.com>
2015-11-07 11:19:51 +0530

    Added formatting based on length

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ from PIL import ImageFont
 
 def imagify(line):
 
-    img = Image.new('RGB', (1080, 720), (255, 255, 255))
+    canvasL = 1080
+    cavasH = 720
+    img = Image.new('RGB', (canvasL, cavasH), (255, 255, 255))
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("Arial/ariali.ttf", 50)
-    # text to imagify
-    margin = 350
-    offset = 300
-    draw.text((margin, offset), line, font=font, fill=(0,0,0,255) )
+    size = 50
+    font = ImageFont.truetype("Arial/ariali.ttf", size)
+    length = len(line)
+    charactersInALine = 38
+    r = length/charactersInALine + 1
+    margin = 50
+    offset = 50
+    for i in range(0,r):
+        draw.text((margin, offset + 50*i), line[charactersInALine*i:charactersInALine*(i+1)], font=font, fill=(0,0,0,255))
+
     img.save('test.png')
 
 if __name__ == "__main__":
