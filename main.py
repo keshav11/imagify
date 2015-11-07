ffa265bf1f258d7e3fa140f36eef4892142de869
Keshav Gupta <keshav1032@gmail.com>
2015-11-07 10:24:56 +0530

    Added command line

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+import sys
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-img = Image.new('RGB', (1080, 720), (255, 255, 255))
-draw = ImageDraw.Draw(img)
-font = ImageFont.truetype("Arial/ariali.ttf", 50)
-# text to imagify
-line = raw_input()
-margin = 350
-offset = 300
-draw.text((margin, offset), line, font=font, fill=(0,0,0,255) )
-img.save('test.png')
+def imagify(line):
+
+    img = Image.new('RGB', (1080, 720), (255, 255, 255))
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("Arial/ariali.ttf", 50)
+    # text to imagify
+    margin = 350
+    offset = 300
+    draw.text((margin, offset), line, font=font, fill=(0,0,0,255) )
+    img.save('test.png')
+
+if __name__ == "__main__":
+    imagify(sys.argv[1])
