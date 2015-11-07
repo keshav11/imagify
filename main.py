f5c5bb9b0c6e968b56f264935ef25c3d9ee6c0b7
Keshav Gupta <keshav1032@gmail.com>
2015-11-07 10:03:56 +0530

    Formatted text

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from PIL import ImageDraw
 from PIL import ImageFont
 
 img = Image.new('RGB', (1080, 720), (255, 255, 255))
-d = ImageDraw.Draw(img)
+draw = ImageDraw.Draw(img)
+font = ImageFont.truetype("Arial/ariali.ttf", 50)
 # text to imagify
-i = raw_input()
-d.text((30, 30), i, fill=(255, 0, 0))
+line = raw_input()
+margin = 350
+offset = 300
+draw.text((margin, offset), line, font=font, fill=(0,0,0,255) )
 img.save('test.png')
